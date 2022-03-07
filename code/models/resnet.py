e4919bbc54a285fcca61b23c7a8ab343bd38e3df
amitrana001 <amitranavsr94@gmail.com>
2022-03-07 01:27:31 +0100

    added resnet trainer no prior and updated dcgan

---
+++ code/models/resnet.py
@@ -0,0 +1,278 @@
+import os
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from torchvision import datasets, models, transforms
+from torchvision.utils import save_image
+
+
+def get_act(act_name, inplace = True):
+    """ Gettign activation given name """
+    assert act_name in ["ReLU", "Sigmoid", "Tanh"]
+    activation = getattr(nn, act_name)
+    return activation(inplace = True)
+
+
+class ResidualBlockEncoder(nn.Module):
+    
+    def __init__(self, input_channels, output_channels, activation = "ReLU", downsample = False):
+
+        super().__init__()
+        self.activation = activation
+
+        if not downsample:
+            output_channels = input_channels
+
+
+        stride = 2 if downsample else 1  
+
+        self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=output_channels, kernel_size=3, padding=1, stride=stride, bias = False)
+        self.bn1 = nn.BatchNorm2d(output_channels)
+        self.relu = get_act(self.activation, inplace=True)
+
+        self.conv2 = nn.Conv2d(in_channels=output_channels, out_channels=output_channels, kernel_size=3, padding=1, stride=1, bias = False)
+        self.bn2 = nn.BatchNorm2d(output_channels)
+
+        self.downsample = None
+        if downsample:
+            self.downsample = nn.Sequential(nn.Conv2d(in_channels=input_channels, out_channels=output_channels, kernel_size=1, stride=2),
+                                          nn.BatchNorm2d(output_channels))
+
+        # self.act = self.activation
+    
+    def forward(self,x):
+
+        y = self.conv1(x)
+        y = self.bn1(y)
+        y = self.relu(y)
+
+        y = self.conv2(y)
+        y = self.bn2(y)
+
+        if self.downsample is not None:
+            x = self.downsample(x)
+
+        out = y+x
+        out = get_act(self.activation, inplace=True)(out)
+        return out
+    
+class Resnet18Encoder(nn.Module):
+    
+    def __init__(self, in_size = (1,64,64), kernels = [64, 128, 256, 512], latent_dim = 128, activation = "ReLU"):
+ 
+        super().__init__()
+
+        self.activation = activation
+        # self.activation =  get_act(activation, inplace=True)
+        self.latent_dim = latent_dim
+        self.in_size = in_size
+        self.input_channels = in_size[0]
+        self.kernels = kernels
+
+        # if self.in_size[1] == 128:
+        #     self.c1 = nn.Sequential(
+        #         nn.Conv2d(in_channels=self.input_channels, out_channels=self.kernels[0], kernel_size=3, padding=1, stride=1, bias = False),
+        #         nn.BatchNorm2d(self.kernels[0]),
+        #         get_act(self.activation, inplace=True),
+        #         nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        #     )
+        # else:
+        self.c1 = nn.Sequential(
+            nn.Conv2d(in_channels=self.input_channels, out_channels=self.kernels[0], kernel_size=3, padding=1, stride=1, bias = False),
+            nn.BatchNorm2d(self.kernels[0]),
+            get_act(self.activation, inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        )
+
+        self.layer1 = self._make_layer(self.kernels[0], self.kernels[0],  downsample=False)
+        self.layer2 = self._make_layer(self.kernels[0], self.kernels[1],  downsample=True)
+        self.layer3 = self._make_layer(self.kernels[1], self.kernels[2], downsample=True)
+        self.layer4 = self._make_layer(self.kernels[2], self.kernels[3], downsample=True)
+        
+        self.layer5 = None
+        if  self.in_size[1] == 128:
+            self.layer5 = self._make_layer(self.kernels[3], self.kernels[3], downsample=True)
+
+        # self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        # self.latent_embed = nn.Linear(512, 2*self.latent_dim )
+
+        self.c6 = nn.Sequential(
+                nn.Conv2d(512, self.latent_dim, 4, 1, 0),
+                nn.BatchNorm2d(self.latent_dim),
+                nn.Tanh()
+        )
+
+    def _make_layer(self, input_channels, output_channels, downsample=False):
+
+        layers = []
+        layers.append(ResidualBlockEncoder(input_channels, output_channels, activation = self.activation, downsample = downsample))
+
+        if downsample:
+            layers.append(ResidualBlockEncoder(output_channels, output_channels, activation = self.activation, downsample = False))
+
+        else:
+            layers.append(ResidualBlockEncoder(input_channels, input_channels, activation = self.activation, downsample = False))
+
+        return nn.Sequential(*layers)
+
+
+    def forward(self, x):
+        
+        #output list for skip connection
+        encoded_skip =[]
+
+        input = self.c1(x)
+        encoded_skip.append(input)
+
+        l1 = self.layer1(input)
+        encoded_skip.append(l1)
+        
+        l2 = self.layer2(l1)
+        encoded_skip.append(l2)
+
+        l3 = self.layer3(l2)
+        encoded_skip.append(l3)
+
+        l4 = self.layer4(l3)
+        encoded_skip.append(l4)
+
+        if self.layer5 is not None:
+            l5 = self.layer5(l4)
+            encoded_skip.append(l5)
+            y = self.c6(l5)
+        else:
+            y = self.c6(l4)
+        # x = self.avgpool(x)
+        # x = x.view(x.size(0), -1)
+        # x = self.latent_embed(x)
+
+        # mu = x[:, :self.latent_dim]
+        # logvar = x[:, self.latent_dim:]
+        
+        return y.view(-1,self.latent_dim), encoded_skip
+
+class ResidualBlockDecoder(nn.Module):
+    def __init__(self, input_channels, output_channels, activation = "ReLU", upsample = False):
+
+        super().__init__()
+        self.activation = activation
+
+        # if not upsample:
+        #   output_channels = input_channels
+
+
+        stride = 2 if upsample else 1  
+
+        self.conv2 = nn.Conv2d(in_channels=input_channels, out_channels=input_channels, kernel_size=3, padding=1, stride=1, bias = False)
+        self.bn2 = nn.BatchNorm2d(input_channels)
+        self.relu = get_act(activation, inplace=True)
+
+        if(stride==1):
+            self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=output_channels, kernel_size=3, padding=1, stride=1, bias = False)
+            self.bn1 = nn.BatchNorm2d(output_channels)
+
+        self.upsample = None
+        if upsample:
+            self.conv1 = nn.ConvTranspose2d(in_channels=input_channels, out_channels=output_channels, kernel_size=3, padding=1,  output_padding=1, stride=2, bias = False)
+            self.bn1 = nn.BatchNorm2d(output_channels)
+            self.upsample = nn.Sequential(nn.ConvTranspose2d(in_channels=input_channels, out_channels=output_channels, kernel_size=3, padding=1,  output_padding=1, stride=2, bias = False),
+                                          nn.BatchNorm2d(output_channels))
+
+        # self.act = self.activation
+
+    def forward(self,x):
+
+        y = self.conv2(x)
+        y = self.bn2(y)
+        y = self.relu(y)
+
+        y = self.conv1(y)
+        y = self.bn1(y)
+
+        if self.upsample is not None:
+            x = self.upsample(x)
+
+        out = y+x
+        out = get_act(self.activation, inplace=True)(out)
+        return out
+    
+class Resnet18Decoder(nn.Module):
+    
+    def __init__(self, out_size = (1,64,64), kernels = [64, 128, 256, 512], latent_dim = 128, activation = "ReLU"):
+ 
+        super().__init__()
+
+        self.activation = activation
+        # self.activation =  get_act(activation, inplace=True)
+        self.latent_dim = latent_dim
+        self.input_channels = out_size[0]
+        self.out_size = out_size 
+        self.kernels = kernels
+
+        # self.inv_latent_embed = nn.Linear(self.latent_dim, 512)
+
+        self.upc1 = nn.Sequential(
+            nn.ConvTranspose2d(self.latent_dim, 512, 4, 1, 0),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True)
+            )
+
+        self.layer5 = None
+        if  self.out_size[1] == 128:
+            self.layer5 = self._make_layer(2*self.kernels[-1], self.kernels[-1], upsample=True)
+
+        self.layer4 = self._make_layer(2*self.kernels[-1], self.kernels[-2],  upsample=True)
+        self.layer3 = self._make_layer(2*self.kernels[-2], self.kernels[-3],  upsample=True)
+        self.layer2 = self._make_layer(2*self.kernels[-3], self.kernels[-4], upsample=True)
+        self.layer1 = self._make_layer(2*self.kernels[-4], self.kernels[-4], upsample=False)
+
+        # self.conv1 = nn.ConvTranspose2d(in_channels=64, out_channels=self.input_channels, kernel_size=3, padding=1, output_padding=1,stride=2, bias = False)
+        # self.up = nn.UpsamplingNearest2d(scale_factor=2)
+
+        self.upc6 = nn.Sequential(
+            nn.UpsamplingNearest2d(scale_factor=2),
+            nn.ConvTranspose2d(4*64, self.input_channels, kernel_size=3, padding=1, stride=1),
+            nn.Sigmoid()
+            )
+
+    def _make_layer(self, input_channels, output_channels, upsample=False):
+
+        layers = []
+        layers.append(ResidualBlockDecoder(input_channels, input_channels, activation = self.activation, upsample = False))
+
+        if upsample:
+            layers.append(ResidualBlockDecoder(input_channels, output_channels, activation = self.activation, upsample = True))
+
+        else:
+            layers.append(ResidualBlockDecoder(input_channels, input_channels, activation = self.activation, upsample = False))
+
+        return nn.Sequential(*layers)
+
+    def forward(self,x):
+
+        # x = self.inv_latent_embed(x)
+        # x = x.view(x.size(0), 512, 1, 1)
+        # x = F.interpolate(x, scale_factor=4)
+        input, encoded_skip = x
+
+        up1 = self.upc1(input.view(-1, self.latent_dim, 1, 1))
+
+        if self.layer5 is not None:
+            up1 = self.layer5(torch.cat([up1, encoded_skip[5]], 1))
+            # encoded_skip.append(x)
+
+        l4 = self.layer4(torch.cat([up1, encoded_skip[4]], 1))
+        l3 = self.layer3(torch.cat([l4, encoded_skip[3]], 1))
+        l2 = self.layer2(torch.cat([l3, encoded_skip[2]], 1))
+        l1 = self.layer1(torch.cat([l2, encoded_skip[1]], 1))
+
+        # x = torch.sigmoid(self.conv1(x))
+        # x = F.interpolate(x, scale_factor=2)
+        out = self.upc6(torch.cat([l1, encoded_skip[0]], 1))
+        # x = x.view(-1, 1, 128, 128)
+        return out
--- code/utils/TrainerNP.py
+++ code/utils/TrainerNP.py
@@ -1,71 +1,59 @@
 from models.dcgan import *
 from models.lstm import *
+from models.resnet import *
 import torch
 import torch.nn as nn
 from tqdm import tqdm
 from utils.visualizations import *
 
-class TrainerFP:
+class TrainerNP:
     """
     Class for initializing network and training it
     """
-    def __init__(self, train_type = "FIxed Prior", batch_size =40, embed_dim=128, hidden_dim=256,
-                latent_dim=10, img_size=64, device="cpu", writer=None):
+    def __init__(self, arch_type = "dcgan", lstm_type = "lstm", batch_size =40, embed_dim=128, hidden_dim=256,
+                 img_size=(1,64,64), device="cpu", writer=None):
         """ Initialzer """
         # assert writer is not None, f"Tensorboard writer not set..."
         
         self.past_frames = 10
         self.future_frames = 10 
-        self.z_dim = latent_dim
         self.g_dim = embed_dim
+        self.img_size = img_size
         self.lr = 0.002
-        self.beta = 0.0001
         self.hidden_dim = hidden_dim
         self.batch_size = batch_size
         self.last_frame_skip = True
         self.device = device
 
-        # if train_type == "Fixed Prior" and img_size==64:
-        self.encoder = DCGANEncoder()
-        self.decoder = DCGANDecoder()
+        if arch_type == "resnet":
+            self.encoder = Resnet18Encoder(in_size=self.img_size)
+            self.decoder = Resnet18Decoder(out_size=self.img_size)
+        elif arch_type == "dcgan":
+            self.encoder = DCGANEncoder(in_size=self.img_size)
+            self.decoder = DCGANDecoder(out_size=self.img_size)
+        
+        if lstm_type == "lstm":
+            self.predictor = predictor_lstm(self.g_dim, self.g_dim, self.hidden_dim, num_layers=2, batch_size=self.batch_size)
+        
         self.encoder = self.encoder.to(device)
         self.decoder = self.decoder.to(device)
-
-        self.predictor = predictor_lstm(self.g_dim + self.z_dim, self.g_dim, self.hidden_dim, num_layers=2, batch_size=self.batch_size)
-        self.posterior = latent_lstm(self.g_dim, self.z_dim, hidden_dim, num_layers=1, batch_size=self.batch_size)
         self.predictor = self.predictor.to(device)
-        self.posterior = self.posterior.to(device)
         # Decay LR by a factor of 0.1 every 5 epochs
         # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.2)
         self.predictor_optimizer = torch.optim.Adam(self.predictor.parameters(), lr=self.lr, betas = (0.9, 0.999))
-        self.posterior_optimizer = torch.optim.Adam(self.posterior.parameters(), lr=self.lr, betas = (0.9, 0.999))
         self.encoder_optimizer = torch.optim.Adam(self.encoder.parameters(), lr=self.lr, betas = (0.9, 0.999))
         self.decoder_optimizer = torch.optim.Adam(self.decoder.parameters(), lr=self.lr, betas = (0.9, 0.999))
 
         self.mse_loss = nn.MSELoss()
-    
-    def kld_loss(self, mu, log_var):
-        """
-        Combined loss function for joint optimization of 
-        prediction and ELBO
-        """
-        # mse = F.mse_loss(predicted, real, reduction="sum")
-    #     recons_loss = F.binary_cross_entropy(recons.view(b_size,-1), target.view(b_size,-1), reduction='sum')
-        elbo = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
-        elbo /= self.batch_size
-        # total_loss = mse + elbo*beta
-        return elbo
 
     def train_one_step(self, x):
         """ Training both models for one optimization step """
         
-        self.predictor.zero_grad()
-        self.posterior.zero_grad()
-        self.encoder.zero_grad()
-        self.decoder.zero_grad()
+        self.predictor_optimizer.zero_grad()
+        self.encoder_optimizer.zero_grad()
+        self.decoder_optimizer.zero_grad()
 
         self.predictor.h, self.predictor.c = self.predictor.init_state(b_size=self.batch_size, device=self.device)
-        self.posterior.h, self.posterior.c = self.posterior.init_state(b_size=self.batch_size, device=self.device)
         # print("hello")
 
         h_seq = [self.encoder(x[i]) for i in range(self.past_frames+self.future_frames)]
@@ -78,31 +66,22 @@ class TrainerFP:
                 h, skip = h_seq[i-1]
             else:
                 h = h_seq[i-1][0]
-            # print("here")
-            # print(h_target.shape)
-            z_t, mu, log_var = self.posterior(h_target)
-            # print("here")
-            # print(z_t.shape)
-            h_pred = self.predictor(torch.cat([h, z_t], 1))
+            h_pred = self.predictor(h)
             x_pred = self.decoder([h_pred, skip])
             mse+=self.mse_loss(x_pred,x[i])
-            kld+=self.kld_loss(mu,log_var)
 
-        total_loss = mse + kld*self.beta
-        total_loss.backward()
+        mse.backward()
 
         self.predictor_optimizer.step()
-        self.posterior_optimizer.step()
         self.encoder_optimizer.step()
         self.decoder_optimizer.step()
 
-        return mse.data.cpu().numpy()/(self.past_frames+self.future_frames), kld.data.cpu().numpy()/(self.past_frames+self.future_frames)
+        return mse.data.cpu().numpy()/(self.past_frames+self.future_frames)
     
     @torch.no_grad()
     def generate_future_sequences(self, test_batch):
         """ Generating a bunch of images using current state of generator """
         self.predictor.eval()
-        self.posterior.eval()
         self.encoder.eval()
         self.decoder.eval()
 
@@ -118,19 +97,13 @@ class TrainerFP:
             else:
                 h, _ = h
             h = h.detach()
-
-            if i< self.past_frames:
-                h_target = self.encoder(test_batch[i])[0].detach()
-                z_t, _, _ = self.posterior(h_target)
-            else:
-                z_t = torch.cuda.FloatTensor(self.batch_size, self.z_dim).normal_()
             
             if i < self.past_frames:
-                self.predictor(torch.cat([h, z_t], 1))
+                self.predictor(h)
                 x_in = test_batch[i]
                 all_gen.append(x_in)
             else:
-                h = self.predictor(torch.cat([h, z_t], 1)).detach()
+                h = self.predictor(h).detach()
                 x_in = self.decoder([h, skip]).detach()
                 pred_seq.append(x_in)
                 gt_seq.append(test_batch[i])
@@ -154,20 +127,16 @@ class TrainerFP:
         save_gif_batch(test_batch, nsamples=5, text = "real", show=False)
         for i in range(num_epochs):
             self.predictor.train()
-            self.posterior.train()
             self.encoder.train()
             self.decoder.train() 
             epoch_mse =0
-            epoch_kld=0
-            epoch_loss =0
 
             progress_bar = tqdm(enumerate(train_loader), total=len(train_loader))
             for _, seqs in progress_bar:
                 seqs = seqs.to(device)
-                mse, kld = self.train_one_step(seqs)
+                mse = self.train_one_step(seqs)
                 epoch_mse+=mse
-                epoch_kld+=kld
-                progress_bar.set_description(f"Epoch {i+1} Iter {niter+1}: mse loss {epoch_mse:.5f}, kld los {epoch_kld: .5f} ")
+                progress_bar.set_description(f"Epoch {i+1} Iter {niter+1}: mse loss {epoch_mse:.5f} ")
                 
                 # write mse and kld losses to tensorboard after every iteration
                 niter+=1
@@ -183,7 +152,6 @@ class TrainerFP:
                 all_gen, gt_seq, pred_seq = self.generate_future_sequences(test_batch)
                 save_pred_gifs(pred_seq, nsamples = 5, text=f"epoch_{i+1}",show=False)
                 save_grid_batch(test_batch, all_gen, nsamples = 5, text=f"grid_epoch_{i+1}",show=False)
-
 
  
                 # 
